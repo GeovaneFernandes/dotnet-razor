ae58b4cb88552c33f11928093186eff8b49e6e0f
Geovane Fernandes <geovanefernandesdois@gmail.com>
2024-12-12 17:47:53 -0300

    41. Add Image Upload To Froala Editor

--- Bloggie.Web/Pages/Admin/Blogs/Edit.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Edit.cshtml.cs
@@ -17,6 +17,9 @@ namespace Bloggie.Web.Pages.Admin.Blogs
         [BindProperty]
         public  BlogPost BlogPost { get; set; }
 
+        [BindProperty]
+        public IFormFile FeaturedImage { get; set; }
+
         public EditModel(IBlogPostRepository blogPostRepository)
         {
             this.blogPostRepository = blogPostRepository;
