f915742837f849c3258650d525937a1197a1d0f2
Geovane Fernandes <geovanefernandesdois@gmail.com>
2024-12-13 15:44:32 -0300

    44. Displaying Blogs and Hero Section

--- Bloggie.Web/Pages/Index.cshtml.cs
+++ Bloggie.Web/Pages/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,16 +8,23 @@ namespace Bloggie.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        
         private readonly ILogger<IndexModel> _logger;
+        private readonly IBlogPostRepository blogPostRepository;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        
+        public List<BlogPost> Blogs { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IBlogPostRepository blogPostRepository)
         {
             _logger = logger;
+            this.blogPostRepository = blogPostRepository;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-
+            Blogs = (await blogPostRepository.GetAllAsync()).ToList();
+            return Page();
         }
     }
 }
