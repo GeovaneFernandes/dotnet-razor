a77c8b89632dc4cba433a01fcf3557c1209c21bb
Geovane Fernandes <geovanefernandesdois@gmail.com>
2024-12-23 15:24:35 -0300

    59. Testing Register Functionality

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -23,6 +23,18 @@ builder.Services.AddDbContext<AuthDbContext>(options =>
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<AuthDbContext>();
 
+builder.Services.Configure<IdentityOptions>(options => 
+{
+    // Default password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+});
+
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, ImageRepositoryCloudinary>();
 builder.Services.AddScoped<ITagRepository, TagRepository>();
