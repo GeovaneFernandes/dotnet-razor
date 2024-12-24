b062c13c2dba9f44f5ab5444156a232bd0843a4d
Geovane Fernandes <geovanefernandesdois@gmail.com>
2024-12-24 17:35:50 -0300

    70. Implement Adding A Like

--- Bloggie.Web/Repositories/BlogPostLikeRepository.cs
+++ Bloggie.Web/Repositories/BlogPostLikeRepository.cs
@@ -1,5 +1,6 @@
 ﻿
 using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Repositories
@@ -12,6 +13,19 @@ namespace Bloggie.Web.Repositories
         {
             this.bloggieDbContext = bloggieDbContext;
         }
+
+        public async Task AddLikeForBlog(Guid blogPostId, Guid userId)
+        {
+            var like = new BlogPostLike
+            {
+                Id = Guid.NewGuid(),
+                BlogPostId = blogPostId,
+                UserId = userId
+            };
+            await bloggieDbContext.BlogPostLike.AddAsync(like);
+            await bloggieDbContext.SaveChangesAsync();
+        }
+
         public async Task<int> GetTotalLikesForBlog(Guid blogPostId)
         {
             return await bloggieDbContext.BlogPostLike
