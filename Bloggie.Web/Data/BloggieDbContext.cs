2e82592ebb58da60627ff4e3b976a2bcca72142b
Geovane Fernandes <geovanefernandesdois@gmail.com>
2024-12-27 14:17:35 -0300

    72. Domain Model For Blog Post Comment And Migration

--- Bloggie.Web/Data/BloggieDbContext.cs
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -9,5 +9,7 @@ namespace Bloggie.Web.Data
         public required DbSet<Tag> Tags { get; set; }
 
         public DbSet<BlogPostLike> BlogPostLike { get; set; }
+
+        public DbSet<BlogPostComment> BlogPostComment { get; set; }
     }
 }
