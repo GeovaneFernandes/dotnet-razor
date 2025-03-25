4a6d503dc75f3dbcbc3958e7cc3166d6f1e8ed5a
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-03-25 10:43:57 -0300

    Add Vue.js integration and configuration files for Razor Pages

---
+++ Bloggie.Web/wwwroot/js/vueComponent.js
@@ -0,0 +1,26 @@
+﻿const app = Vue.createApp({
+    data() {
+        return {
+            message: "Hello from Vue inside Razor Page!"
+        };
+    }
+});
+
+app.component('vue-component', {
+    template: `<div>
+                 <h3>{{ message }}</h3>
+                 <button @click="updateMessage">Click Me</button>
+               </div>`,
+    data() {
+        return {
+            message: "Vue Component inside Razor!"
+        };
+    },
+    methods: {
+        updateMessage() {
+            this.message = "You clicked the button!";
+        }
+    }
+});
+
+app.mount("#vue-app");
